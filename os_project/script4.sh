bae21751ae404c928003112a4c31afbe4ac843d8
Telejean <raresvisanandrei@gmail.com>
2023-05-10 05:32:35 -0400

    lucrat la script4

--- os_project/script4.sh
+++ os_project/script4.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
 optime=$(date +%c) #timpul de afisat in logfile
+
+echo 'See memory usage:    (Press q to exit) '
+input='a'
+max=0;
+
+while true; do
+	read -t 1.5 input ;
+	if [[ $input = "q" ]]
+		then break
+	else
+		mem=$(inxi -c | grep -o "Mem.*")
+		max=$mem | grep -o '(.....)'
+		time=$(date | grep -o '..:..:..')
+		echo "$mem $time $max"
+	fi
+done

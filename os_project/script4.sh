ad95a0f44a648a657615cca1932c05ed484ddf6e
Telejean <raresvisanandrei@gmail.com>
2023-05-11 21:23:36 +0300

    Script4 gata

--- os_project/script4.sh
+++ os_project/script4.sh
@@ -4,15 +4,20 @@ optime=$(date +%c) #timpul de afisat in logfile
 echo 'See memory usage:    (Press q to exit) '
 input='a'
 max=0;
-
+last_max=0;
 while true; do
 	read -t 1.5 input ;
 	if [[ $input = "q" ]]
 		then break
 	else
 		mem=$(inxi -c | grep -o "Mem.*")
-		max=$mem | grep -o '(.....)'
+		max=$(echo $mem | grep -o "....%") 
+	      	if (( ${max%???} > $last_max )); then
+			last_max=${max%???};	
+		fi
+			
 		time=$(date | grep -o '..:..:..')
 		echo "$mem $time $max"
 	fi
 done
+echo "Most RAM used: $last_max %"

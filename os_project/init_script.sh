c588efdd543f31deb9a9b62e84650d034d25edf2
Telejean <raresvisanandrei@gmail.com>
2023-05-12 04:14:58 -0400

    adaugat init_script

---
+++ os_project/init_script.sh
@@ -0,0 +1,10 @@
+echo "This scipt will initialize all required permisions and install the inxi pachet"
+
+chmod +x main_script.sh
+chmod +x script1.sh
+chmod +x script2.sh
+chmod +x script3.sh
+chmod +x script4.sh
+
+sudo apt install inxi
+

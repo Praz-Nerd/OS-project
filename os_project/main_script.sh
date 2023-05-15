b82bb7607bc99c727e64407a90899717383a9946
Praz-Nerd <stefan.vladescu2012@gmail.com>
2023-05-15 18:48:40 +0300

    removing profanity

--- os_project/main_script.sh
+++ os_project/main_script.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 clear
-touch ./logfile.txt #logul nostru (work in progress, poate vrea clim nume personalizat pt fisier)
+touch ./logfile.txt 
 printf "\nThe project\nChoose one of these options:\n1) do one\n2) do two\n3) do three\n4) do four\n5) exit and save with log\n6) exit and don't save\n"
 read -p "Your option: " sel
-while ((sel != 5)) && ((sel != 6)); #leading file algorithm (kill me)
+while ((sel != 5)) && ((sel != 6)); 
 do
     clear
     if ((sel > 0)) && ((sel < 5));
     then
         printf "\nYour choice is %d \n" $sel
         
-        case $sel in #gen switch din C dar mai retardat, apelam scripturile secundare pt fiecare (modularity FTW)
-            1)       #de tip REGEX_PATTERN) (mai bun decat switchul din C (poate nu chiar asa retardat))
+        case $sel in 
+            1)       
                 ./script1.sh
             ;;
             2)
@@ -38,7 +38,7 @@ done
 
 if ((sel == 6));
 then
-    rm ./logfile.txt #stergem logul, dupa caz
+    rm ./logfile.txt 
 elif((sel == 5));
 then
     echo "Log file saved"
